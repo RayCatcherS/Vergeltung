096f193d581f75cbdf25bad1d6f99211310bd632
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2022-03-07 17:25:29 +0100

    Implementata rotazione character dipendente dal sistema di mira

--- Assets/assets/Entities/characters/base_character/script/CharacterMovement.cs
+++ Assets/assets/Entities/characters/base_character/script/CharacterMovement.cs
@@ -12,16 +12,17 @@ public class CharacterMovement : MonoBehaviour
 
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float rotationSpeed = 20f;
-    public Transform characterModelrotationTransform;
+    public GameObject characterModel;
     public Transform aimTransform;
 
-    [SerializeField] private Vector3 _rotationTarget;
+    [SerializeField] private Vector3 rotationAimInput;
+    [SerializeField] private Vector2 characterModelrotation;
 
 
     private Animator animator; //animator del character
 
     void Awake() {
-        animator = GetComponent<Animator>();
+        animator = characterModel.gameObject.GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -58,8 +59,8 @@ public class CharacterMovement : MonoBehaviour
 
 
         // setta valori animazione partendo dal _movementAnimationVelocity
-        float velocityX = Vector3.Dot(_movementAnimationVelocity, transform.right);
-        float velocityZ = Vector3.Dot(_movementAnimationVelocity, transform.forward);
+        float velocityX = Vector3.Dot(_movementAnimationVelocity, characterModel.transform.right);
+        float velocityZ = Vector3.Dot(_movementAnimationVelocity, characterModel.transform.forward);
 
         animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
         animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
@@ -70,20 +71,144 @@ public class CharacterMovement : MonoBehaviour
     /// Metodo con cui viene applicato uno spostamento sul character
     /// </summary>
     /// <param name="_2Drotate">Coppia di valori che rappresenta i valori
-    /// in input della rotazione del character. I valori(x, y) sono Clampati tra -1 e 1</param>
+    /// in input della rotazione dell'aim del character. I valori(x, y) sono Clampati tra -1 e 1</param>
+    /// vengono inoltre calcolati i range per ruotare l'intero character
     public void rotateCharacter(Vector2 _2Drotate) {
-        Vector3 rotationTarget; // vettore rotazione target
+        Vector3 rotationAimTarget; // vettore rotazione target
 
         // clamp dei valori passati 
-        rotationTarget = new Vector3(
+        rotationAimTarget = new Vector3(
             Mathf.Clamp(_2Drotate.x, NEGATIVE_ROTATION_CLAMP, POSITIVE_ROTATION_CLAMP),
             Mathf.Clamp(_2Drotate.y, NEGATIVE_ROTATION_CLAMP, POSITIVE_ROTATION_CLAMP),
             0f);
+        rotationAimInput = rotationAimTarget;
 
-        if (rotationTarget.magnitude > 0) {
-            // ruota player partendo dalle coordinate rotazione (utilizzando la funzione lerp)
-            aimTransform.rotation = Quaternion.Lerp(aimTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(rotationTarget.x, rotationTarget.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        if (rotationAimTarget.magnitude > 0) {
+            // ruota aim character partendo dalle coordinate rotazione (utilizzando la funzione lerp)
+            aimTransform.rotation = Quaternion.Lerp(aimTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(rotationAimTarget.x, rotationAimTarget.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
         }
+
+
+
+        Vector2 characterRotation = new Vector2();
+
+        if (
+            rotationAimTarget.x > -0.707f && rotationAimTarget.y > 0.707f &&
+            rotationAimTarget.x < 0.707f && rotationAimTarget.y > 0.707f
+        ) {
+            characterRotation.x = 0;
+            characterRotation.y = 1;
+
+            characterModelrotation = characterRotation;
+            characterModel.transform.rotation = 
+                Quaternion.Lerp(characterModel.transform.rotation,
+                Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0),
+                Time.deltaTime * rotationSpeed);
+        } else if (
+            rotationAimTarget.x > 0.707f && rotationAimTarget.y < 0.707f &&
+            rotationAimTarget.x > 0.707f && rotationAimTarget.y > -0.707f
+        ) {
+            characterRotation.x = 1f;
+            characterRotation.y = 0f;
+
+            characterModelrotation = characterRotation;
+            characterModel.transform.rotation =
+                Quaternion.Lerp(characterModel.transform.rotation,
+                Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0),
+                Time.deltaTime * rotationSpeed);
+        } else if(
+            rotationAimTarget.x < 0.707f && rotationAimTarget.y < -0.707f &&
+            rotationAimTarget.x > -0.707f && rotationAimTarget.y < -0.707f
+        ) {
+            characterRotation.x = 0f;
+            characterRotation.y = -1f;
+
+            characterModelrotation = characterRotation;
+            characterModel.transform.rotation =
+                Quaternion.Lerp(characterModel.transform.rotation,
+                Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0),
+                Time.deltaTime * rotationSpeed);
+        } else if (
+            rotationAimTarget.x < -0.707f && rotationAimTarget.y > -0.707f &&
+            rotationAimTarget.x < -0.707f && rotationAimTarget.y < 0.707f
+        ) {
+            characterRotation.x = -1f;
+            characterRotation.y = 0f;
+
+            characterModelrotation = characterRotation;
+            characterModel.transform.rotation =
+                Quaternion.Lerp(characterModel.transform.rotation,
+                Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0),
+                Time.deltaTime * rotationSpeed);
+        }
+        /*if(rotationAimTarget.x == -1 && rotationAimTarget.y == 0) { // (-1, 0)
+
+            characterRotation.x = -1f;
+            characterRotation.y = 0f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if (// 1 quadrante, prima porzione del quadrante 
+            rotationAimTarget.x < -0.707f && rotationAimTarget.y < 0.707f &&
+            rotationAimTarget.x >= -1 && rotationAimTarget.y > 0
+         ) {
+
+            characterRotation.x = -0.866f;
+            characterRotation.y = 0.5f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if (// 1 quadrante, seconda porzione del quadrante 
+            rotationAimTarget.x < 0f && rotationAimTarget.y <= 1f &&
+            rotationAimTarget.x > -0.707f && rotationAimTarget.y > 0.707f
+        ) {
+
+            characterRotation.x = -0.5f;
+            characterRotation.y = 0.66f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if(rotationAimTarget.x == 0 && rotationAimTarget.y == 1) { // (0,1)
+            
+            characterRotation.x = 0f;
+            characterRotation.y = 1f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if(
+            rotationAimTarget.x > 0 && rotationAimTarget.y <= 1 &&
+            rotationAimTarget.x < 0.707f && rotationAimTarget.y > 0.707f
+        ) {// 2 quadrante, prima porzione del quadrante
+
+            characterRotation.x = 0.5f;
+            characterRotation.y = 0.866f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if(
+            rotationAimTarget.x > 0.707f && rotationAimTarget.y < 0.707f &&
+            rotationAimTarget.x <= 1f && rotationAimTarget.y > 0
+        ) {// 2 quadrante, seconda porzione del quadrante
+            characterRotation.x = 0.866f;
+            characterRotation.y = 0.5f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        } else if(rotationAimTarget.x == 1 && rotationAimTarget.y == 0) {
+            characterRotation.x = 1;
+            characterRotation.y = 0f;
+
+            characterModelrotation = characterRotation;
+            characterModelrotationTransform.rotation = Quaternion.Lerp(characterModelrotationTransform.rotation, Quaternion.Euler(0, 360 - (Mathf.Atan2(characterRotation.x, characterRotation.y) * Mathf.Rad2Deg * -1), 0), Time.deltaTime * rotationSpeed);
+        }*/
+
+
+    }
+
+
+    void OnGUI() {
+        GUI.TextArea(new Rect(0, 0, 100, 40), "input rotazione \n" + rotationAimInput.ToString(), 200);
+        GUI.TextArea(new Rect(0, 40, 150, 40), "rotazione character \n" + characterModelrotation.ToString(), 200);
     }
 
     void OnDrawGizmos() {
@@ -91,3 +216,6 @@ public class CharacterMovement : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, 5);
     }
 }
+
+    
+    
