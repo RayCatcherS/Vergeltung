0a20f21f656d6344461eef7a2ece684f1e6ff02e
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2022-06-03 18:35:29 +0200

    Vari fix animator

--- Assets/AlarmAlertUIController.cs
+++ Assets/AlarmAlertUIController.cs
@@ -15,74 +15,101 @@ public class AlarmAlertUIController : MonoBehaviour
 
 
     public void potentialWantedAlarmOn() {
+        wantedAnimator.gameObject.SetActive(true);
+
+
         wantedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
         wantedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
         wantedAnimator.ResetTrigger(ALARM_EXIT);
-
-        wantedAnimator.gameObject.SetActive(true);
+        
         wantedAnimator.SetTrigger(ALARM_ON_TRIGGER);
     }
     public void potentialWantedAlarmOff() {
-        wantedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
-        wantedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
-        wantedAnimator.ResetTrigger(ALARM_EXIT);
 
-        wantedAnimator.SetTrigger(ALARM_OFF_TRIGGER);
-        wantedAnimator.SetTrigger(ALARM_EXIT);
+        if (lockPickingAnimator.gameObject.activeSelf) {
+
+            wantedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
+            wantedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
+            wantedAnimator.ResetTrigger(ALARM_EXIT);
+
+            wantedAnimator.SetTrigger(ALARM_OFF_TRIGGER);
+            wantedAnimator.SetTrigger(ALARM_EXIT);
+        }
+        
     }
 
 
 
     public void potentialVisiblyArmedAlarmOn() {
+        visiblyArmedAnimator.gameObject.SetActive(true);
+
+
         visiblyArmedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
         visiblyArmedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
         visiblyArmedAnimator.ResetTrigger(ALARM_EXIT);
-
-        visiblyArmedAnimator.gameObject.SetActive(true);
+        
         visiblyArmedAnimator.SetTrigger(ALARM_ON_TRIGGER);
     }
     public void potentialVisiblyArmedAlarmOff() {
-        visiblyArmedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
-        visiblyArmedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
-        visiblyArmedAnimator.ResetTrigger(ALARM_EXIT);
+        if (lockPickingAnimator.gameObject.activeSelf) {
 
-        visiblyArmedAnimator.SetTrigger(ALARM_OFF_TRIGGER);
-        visiblyArmedAnimator.SetTrigger(ALARM_EXIT);
+            visiblyArmedAnimator.ResetTrigger(ALARM_ON_TRIGGER);
+            visiblyArmedAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
+            visiblyArmedAnimator.ResetTrigger(ALARM_EXIT);
+
+            visiblyArmedAnimator.SetTrigger(ALARM_OFF_TRIGGER);
+            visiblyArmedAnimator.SetTrigger(ALARM_EXIT);
+        }
+        
     }
 
 
 
     public void potentialProhibitedAreaAlarmOn() {
+
+        
+
+        prohibitedAreaAnimator.gameObject.SetActive(true);
+
         prohibitedAreaAnimator.ResetTrigger(ALARM_ON_TRIGGER);
         prohibitedAreaAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
         prohibitedAreaAnimator.ResetTrigger(ALARM_EXIT);
 
-        prohibitedAreaAnimator.gameObject.SetActive(true);
         prohibitedAreaAnimator.SetTrigger(ALARM_ON_TRIGGER);
     }
     public void potentialProhibitedAreaAlarmOff() {
-        prohibitedAreaAnimator.ResetTrigger(ALARM_ON_TRIGGER);
-        prohibitedAreaAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
-        prohibitedAreaAnimator.ResetTrigger(ALARM_EXIT);
 
-        prohibitedAreaAnimator.SetTrigger(ALARM_OFF_TRIGGER);
-        prohibitedAreaAnimator.SetTrigger(ALARM_EXIT);
+        if (prohibitedAreaAnimator.gameObject.activeSelf) {
+            prohibitedAreaAnimator.ResetTrigger(ALARM_ON_TRIGGER);
+            prohibitedAreaAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
+            prohibitedAreaAnimator.ResetTrigger(ALARM_EXIT);
+
+            prohibitedAreaAnimator.SetTrigger(ALARM_OFF_TRIGGER);
+            prohibitedAreaAnimator.SetTrigger(ALARM_EXIT);
+        }
+        
     }
 
     public void potentialLockPickingAlarmOn() {
+        lockPickingAnimator.gameObject.SetActive(true);
+
+
         lockPickingAnimator.ResetTrigger(ALARM_ON_TRIGGER);
         lockPickingAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
         lockPickingAnimator.ResetTrigger(ALARM_EXIT);
-
-        lockPickingAnimator.gameObject.SetActive(true);
+        
         lockPickingAnimator.SetTrigger(ALARM_ON_TRIGGER);
     }
     public void potentialLockPickingAlarmOff() {
-        lockPickingAnimator.ResetTrigger(ALARM_ON_TRIGGER);
-        lockPickingAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
-        lockPickingAnimator.ResetTrigger(ALARM_EXIT);
 
-        lockPickingAnimator.SetTrigger(ALARM_OFF_TRIGGER);
-        lockPickingAnimator.SetTrigger(ALARM_EXIT);
+        if (lockPickingAnimator.gameObject.activeSelf) {
+
+            lockPickingAnimator.ResetTrigger(ALARM_ON_TRIGGER);
+            lockPickingAnimator.ResetTrigger(ALARM_OFF_TRIGGER);
+            lockPickingAnimator.ResetTrigger(ALARM_EXIT);
+
+            lockPickingAnimator.SetTrigger(ALARM_OFF_TRIGGER);
+            lockPickingAnimator.SetTrigger(ALARM_EXIT);
+        }
     }
 }
