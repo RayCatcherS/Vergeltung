aac37344fda1bb29bcf77a2ad4e2cf0dac2b2c98
Stefano <romanelli1996@gmail.com>
2023-11-17 14:13:10 +0100

    ready for magic lightmap switcher

--- Assets/scripts/Environment/LightMapSwitcher.cs
+++ Assets/scripts/Environment/LightMapSwitcher.cs
@@ -1,36 +1,39 @@
+using MagicLightmapSwitcher;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightMapSwitcher : MonoBehaviour
-{
-    public Texture2D[] lightmaps; // Array contenente le diverse lightmaps bakeate
+using UnityEngine.SceneManagement;
 
-    void Start() {
+namespace MagicLightmapSwitcher {
+    public class LightMapSwitcher : MonoBehaviour {
 
-        LightmapData[] lightmapData = new LightmapData[lightmaps.Length];
-
-
-        // Assegna le lightmaps dal tuo array a quelle nella scena
-        for(int i = 0; i < lightmaps.Length; i++) {
-
-            lightmapData[i] = new LightmapData();
-            lightmapData[i].lightmapColor = lightmaps[i];
-            
+        void Start() {
+            SwitchToLightmap();
 
         }
 
-        LightmapSettings.lightmaps = lightmapData;
-    }
+        public void SwitchToLightmap() {
 
-    // Metodo per cambiare la lightmap attiva durante l'esecuzione
-    public void SwitchToLightmap(int index) {
-        if(index >= 0 && index < lightmaps.Length) {
-            LightmapSettings.lightmaps[index].lightmapColor = lightmaps[index];
-            Debug.Log("switched");
-        } else {
-            Debug.LogError("Indice lightmap non valido.");
+            // switch lightmaps
+            /*MagicLightmapSwitcher lightmapSwitcher;
+            RuntimeAPI runtimeAPI = new RuntimeAPI();
+
+            List<StoredLightingScenario> availableScenarios = new List<StoredLightingScenario>();*/
+
+
+            //MagicLightmapSwitcher.UpdateLightingScenarios(SceneManager.GetActiveScene().name, true);
+
+
+
+            //StoredLightingScenario
+            string sceneName = SceneManager.GetActiveScene().name;
+            /*lightmapSwitcher = runtimeAPI.GetSwitcherSource(sceneName);
+            Debug.Log(lightmapSwitcher.sceneLightmapDatas.Count);*/
+            //Switching.LoadLightingData(lightmapSwitcher, 0, Switching.LoadMode.Asynchronously);
+
         }
     }
 }
+
